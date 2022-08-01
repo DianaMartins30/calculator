16568ad92934e547c9fc9634a49496ac4a9cdd4a
Diana Martins <diana30martins30@gmail.com>
2022-08-01 17:25:26 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col max-w-md items-center">
+    <InputField v-model="value" />
+    <div>Total: {{ total }}</div>
+    <div>Value: {{ value }}</div>
+    <div class="flex ">
+      <div class="grid grid-cols-4 gap-4 mx-4">
+        <div v-for="calculatorKey in calculatorKeys" :key="calculatorKey" @click="pressed(calculatorKey)"
+          class="bg-red-400 p-6 cursor-pointer">{{ calculatorKey }}</div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputField from './components/InputField.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InputField
+  },
+  data() {
+    return {
+      value: 0,
+      total: 0,
+      calculatorKeys: ['(', ')', '%', 'AC', 7, 8, 9, '/', 4, 5, 6, 'x', 1, 2, 3, '-', 0, '.', '=', '+'],
+    }
+  },
+  methods: {
+
+    pressed(calculatorKey) {
+      switch (calculatorKey) {
+        case '+':
+          console.log(calculatorKey)
+          this.sum();
+          break;
+        case '-':
+          console.log(calculatorKey)
+          this.sub();
+          break;
+        case 'x':
+          console.log(calculatorKey)
+          this.mul();
+          break;
+        default:
+      }
+    },
+
+    sum() {
+      this.total += this.value;
+    },
+    sub() {
+      this.total -= this.value;
+    },
+    mul() {
+      this.total *= this.value;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/InputField.vue
@@ -0,0 +1,17 @@
+<template>
+    <input type="number" :value="value" @input="$emit('update: value', $event.target.value)">
+</template>
+
+<script>
+export default {
+    name: 'InputField',
+
+    props: {
+        value: {
+            default: 0
+        }
+    }
+
+
+}
+</script>
